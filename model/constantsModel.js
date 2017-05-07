1303dfc1eab0800e2eabb39b128d62e93e472cda
KL44S <agustinbinci@hotmail.com>
2017-05-07 19:58:21 -0300

    Rendimiento del código no puede ser mayor que 1

--- model/constantsModel.js
+++ model/constantsModel.js
@@ -11,6 +11,7 @@
 				"lessThanFieldErrorMessage": "este campo no puede ser menor a ",
 				"greaterThanFieldErrorMessage": "este campo no puede ser mayor a ",
 				"separator": "-",
+				"maxPerformanceAllowed": 1,
 				"codePosibleSymbols": [
 								String.fromCharCode(48), String.fromCharCode(49), String.fromCharCode(50), String.fromCharCode(51), String.fromCharCode(52), String.fromCharCode(53), String.fromCharCode(54), 
 								String.fromCharCode(55), String.fromCharCode(56), String.fromCharCode(57), String.fromCharCode(65), String.fromCharCode(66), String.fromCharCode(67), String.fromCharCode(68), 
